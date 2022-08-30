4511b9b453ffe0d4d2476c401aa96ad7770f7cbe
SAM <samuel.mera.ricardo@gmail.com>
2022-08-30 15:06:51 -0600

    added endpoints & handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,87 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func PowerOn(context *gin.Context) error {
+
+	return nil
+}
+
 func Standby(context *gin.Context) error {
 
+	return nil
+}
+
+func GetPower(context *gin.Context) error {
+
+	return nil
+}
+
+func SwitchInput(context *gin.Context) error {
+
+	return nil
+}
+
+func SetVolume(context *gin.Context) error {
+
+	return nil
+}
+
+func VolumeUnmute(context *gin.Context) error {
+
+	return nil
+}
+
+func VolumeMute(context *gin.Context) error {
+
+	return nil
+}
+
+func setMute(context *gin.Context) error {
+
+	return nil
+}
+
+func BlankDisplay(context *gin.Context) error {
+
+	return nil
+}
+
+func UnblankDisplay(context *gin.Context) error {
+
+	return nil
+}
+
+func GetVolume(context *gin.Context) error {
+
+	return nil
+}
+
+func GetInput(context *gin.Context) error {
+
+	return nil
+}
+
+func GetInputList(context *gin.Context) error {
+
+	return nil
+}
+
+func GetMute(context *gin.Context) error {
+
+	return nil
+}
+
+func GerBlank(context *gin.Context) error {
+
+	return nil
+}
+
+func GetHardwareInfo(context *gin.Context) error {
+
+	return nil
+}
+
+func GetActiveSignal(context *gin.Context) error {
+
+	return nil
 }
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/byuoitav/clevertouch-control/handlers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,11 +19,26 @@ func main() {
 
 	// action endpoints
 	write := router.Group("/write")
-	write.GET("/:address/power/standby")
+	write.GET("/:address/power/on", handlers.PowerOn)
+	write.GET("/:address/power/standby", handlers.Standby)
+	write.GET("/:address/input/:port", handlers.SwitchInput)
+	write.GET("/:address/volume/set/:value", handlers.SetVolume)
+	write.GET("/:address/volume/mute", handlers.VolumeMute)
+	write.GET("/:address/volume/unmute", handlers.VolumeUnmute)
+	write.GET("/:address/display/blank", handlers.BlankDisplay)
+	write.GET("/:address/display/unblank", handlers.UnblankDisplay)
 
 	// status endpoints
 	read := router.Group("/read")
 	read.GET("/:address/input/current")
+	read.GET("/:address/power/status", handlers.GetPower)
+	read.GET("/:address/input/current", handlers.GetInput)
+	read.GET("/:address/input/list", handlers.GetInputList)
+	read.GET("/:address/active/:port", handlers.GetActiveSignal)
+	read.GET("/:address/volume/level", handlers.GetVolume)
+	read.GET("/:address/volume/mute/status", handlers.GetMute)
+	read.GET("/:address/display/status", handlers.GetBlank)
+	read.GET("/:address/hardware", handlers.GetHardwareInfo)
 
 	server := &http.Server{
 		Addr:           port,
